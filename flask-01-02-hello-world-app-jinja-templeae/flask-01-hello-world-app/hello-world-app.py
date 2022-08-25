a28e9d4b32ea78387b9d4d77c6e919b7891ce451
unknown <https://github.com/rrafetdemir>
2022-08-25 11:35:09 +0200

    flask-01 and 02 files added

---
+++ flask-01-02-hello-world-app-jinja-templeae/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return "<h1>Hello World Ramazan Rafet this is from Clarusway DE</h1>"
+
+
+@app.route('/second')
+def second():
+    return "This is the second page"
+
+@app.route('/third/subthird')
+def third():
+    return "This is the subpage third page"
+
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f"Id of thids page is {id}"
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask-01-02-hello-world-app-jinja-templeae/flask-02-Jinja_Template/jinja.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return render_template('index.html', number1=10, number2=20)
+
+
+@app.route('/mult')
+def number():
+    x=15
+    y=20
+    return render_template('body.html', num1=x, num2=y, mult=x*y)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=False)
